a82be3ac57ba16c45078dbda0040257f131ba58c
deechou <dchou1@gmail.com>
2024-01-12 16:46:40 -0500

    Added guessing into a loop

--- target/debug/deps/guessing_game.d
+++ target/debug/deps/guessing_game.d
@@ -1,5 +1,5 @@
-C:\Users\echou\Documents\Projects\Rust\guessing_game\target\debug\deps\guessing_game.exe: src\main.rs
+C:\Users\echou\Documents\Projects\RustTutorials\rust-guessing-game\target\debug\deps\guessing_game.exe: src\main.rs
 
-C:\Users\echou\Documents\Projects\Rust\guessing_game\target\debug\deps\guessing_game.d: src\main.rs
+C:\Users\echou\Documents\Projects\RustTutorials\rust-guessing-game\target\debug\deps\guessing_game.d: src\main.rs
 
 src\main.rs:
--- target/debug/guessing_game.d
+++ target/debug/guessing_game.d
@@ -1,1 +1,1 @@
-C:\Users\echou\Documents\Projects\Rust\guessing_game\target\debug\guessing_game.exe: C:\Users\echou\Documents\Projects\Rust\guessing_game\src\main.rs
+C:\Users\echou\Documents\Projects\RustTutorials\rust-guessing-game\target\debug\guessing_game.exe: C:\Users\echou\Documents\Projects\RustTutorials\rust-guessing-game\src\main.rs
